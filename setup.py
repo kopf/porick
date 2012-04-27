8f4e5db7af67668c47e76077ca6db96da1f9a8d2
Aengus Walton <ventolin@gmail.com>
2012-04-27 23:40:43 +0200

    Set SQLAlchemy ver

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     url='',
     install_requires=[
         "Pylons>=1.0.1rc1",
-        "SQLAlchemy>=0.5",
+        "SQLAlchemy==0.6.8",
     ],
     setup_requires=["PasteScript>=1.6.3"],
     packages=find_packages(exclude=['ez_setup']),
