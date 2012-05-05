11206e8237dbd72081ef39c34adc7e51c0e2d32f
Aengus Walton <ventolin@gmail.com>
2012-05-05 23:48:57 +0200

    Re-arrange voting controller a little

--- porick/controllers/vote.py
+++ porick/controllers/vote.py
@@ -14,23 +14,26 @@ class VoteController(BaseController):
 
     @jsonify
     def vote(self, direction, quote_id):
-        if request.environ['REQUEST_METHOD'] not in ['PUT', 'DELETE']:
-            abort(405)
+        if request.environ['REQUEST_METHOD'] == 'PUT':
+            quote = db.query(Quote).filter(Quote.id == quote_id).first()
+            if not quote:
+                return {'msg': 'Invalid quote ID',
+                        'status': 'error'}
 
-        quote = db.query(Quote).filter(Quote.id == quote_id).first()
-        if not quote:
-            return {'msg': 'Invalid quote ID',
-                    'status': 'error'}
+            if direction == 'up':
+                quote.rating += 1
+            elif direction == 'down':
+                quote.rating -= 1
+            else:
+                return {'msg': 'Invalid vote direction',
+                        'status': 'error'}
 
-        if direction == 'up':
-            quote.rating += 1
-        elif direction == 'down':
-            quote.rating -= 1
+            quote.votes += 1
+            db.commit()
+            return {'status': 'success',
+                    'msg': 'Vote cast!'}
+        elif request.environ['REQUEST_METHOD'] == 'DELETE':
+            # delete the appropriate vote
+            pass
         else:
-            return {'msg': 'Invalid vote direction',
-                    'status': 'error'}
-
-        quote.votes += 1
-        db.commit()
-        return {'status': 'success',
-                'msg': 'Vote cast!'}
+            abort(405)
