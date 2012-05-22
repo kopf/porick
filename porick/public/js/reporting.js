47c7c71c96598a99674c37e6895f9a0406303cdd
Aengus Walton <ventolin@gmail.com>
2012-05-22 22:47:20 +0200

    Added a first version of some js for reporting quotes

---
+++ porick/public/js/reporting.js
@@ -0,0 +1,21 @@
+function setupReportingClickHandlers() {
+    /**
+     * Assign click handlers to all favourite buttons.
+     */
+    $('.report').click(function() { 
+    	var quote_id = $(this).data('quote_id');
+        if(! $(this).hasClass('reported')) {
+        	report(quote_id, $(this));
+        }
+    });
+}
+
+function report(quote_id, button) {
+	$.ajax({
+        url: '/api/v1/report/' + quote_id,
+        type: 'PUT',
+        success: function(data, status, jqXHR){
+            button.addClass(data['status'] + ' reported');
+        }
+    });
+}
