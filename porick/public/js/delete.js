658bfff3012a5df29cc83cdb9d7ac5e64239eb83
Aengus Walton <ventolin@gmail.com>
2012-05-24 00:35:03 +0200

    Add confirmation to deletion

--- porick/public/js/delete.js
+++ porick/public/js/delete.js
@@ -5,15 +5,21 @@ function setupDeleteClickHandlers() {
     $('.delete').click(function() { 
         var quote_id = $(this).data('quote_id');
         var button = $(this);
-        $.ajax({
-            url: '/api/v1/delete/' + quote_id,
-            type: 'DELETE',
-            success: function(data, status, jqXHR){
-                button.addClass(data['status'] + ' deleted');
-                if(data['status'] === 'success') {
-                    button.parent().parent().parent().parent().parent().fadeOut('slow');
-                }
+        if(confirm("Do you really want to delete this quote?")) {
+            delete_quote(quote_id, button);
+        }
+    });
+}
+
+function delete_quote(quote_id, button) {
+    $.ajax({
+        url: '/api/v1/delete/' + quote_id,
+        type: 'DELETE',
+        success: function(data, status, jqXHR){
+            button.addClass(data['status'] + ' deleted');
+            if(data['status'] === 'success') {
+                button.parent().parent().parent().parent().parent().fadeOut('slow');
             }
-        });
+        }
     });
 }
