73e67cac0a2bd45211fbca1f2496b4e4a37d74c1
Aengus Walton <ventolin@gmail.com>
2012-06-15 19:02:08 +0200

    Make migrate_db.sh script work in any environment, not just TCD nutsac's

--- migrate_db.sh
+++ migrate_db.sh
@@ -4,14 +4,22 @@
 # Insert the MySQL password for porick here
 PWD=''
 
-cat <<EOF | mysql -u root
+# Insert the Chirpy! database name here
+CHIRPYDB='chirpy'
+
+mysqldump -u root -p $CHIRPYDB --tables chirpy_tags chirpy_quote_tag chirpy_quotes > tmp_dump.sql
+
+cat <<EOF | mysql -u root -p
 CREATE DATABASE IF NOT EXISTS porick;
 GRANT ALL PRIVILEGES ON porick.* TO 'porick'@'localhost' IDENTIFIED BY '$PWD';
 FLUSH PRIVILEGES;
 EOF
 
-cat <<EOF | mysql -u root porick
-$(mysqldump -u root u_chirpy --tables chirpy_tags chirpy_quote_tag chirpy_quotes)
+mysql -u root -p porick < tmp_dump.sql
+
+rm -f tmp_dump.sql
+
+cat <<EOF | mysql -u root -p porick
 RENAME TABLE chirpy_tags TO tags;
 RENAME TABLE chirpy_quotes TO quotes;
 RENAME TABLE chirpy_quote_tag to quote_to_tag;
